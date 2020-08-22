232e68fd049ebbc7ad7aeea6963d6e281a391e65
Juangui Restrepo <juangui.rp@gmail.com>
2020-08-22 15:13:46 -0500

    dev: Using property initializers.

--- src/index.js
+++ src/index.js
@@ -92,4 +92,40 @@ const App3 = () => (
     </div>
 )
 
-ReactDOM.render(<App3 />, document.getElementById('root'))
+//ReactDOM.render(<App3 />, document.getElementById('root'))
+
+// => Non-standard... Property Initializers
+class SimpleAnonymousThreatComponent extends React.Component {
+    state = {
+        remmainingTime: 1000
+    }
+
+    updateRemainingTime = () => this.setState({
+        remmainingTime: this.state.remmainingTime - 5
+    })
+
+
+    render() {
+        return (
+            <div>
+                <h1>We 'have class' and simplicity:</h1>
+                <h2>Hello Mr. {this.props.name}!</h2>
+                <p>We go to your location in {this.props.city} city.</p>
+                <p>Please wait for our arrival.</p>
+                <button onClick={this.updateRemainingTime}>Update remaining time</button>
+                <p>Remaining time of arrival: {this.state.remmainingTime} seconds.</p>
+                <hr />
+            </div>
+        )
+    }
+}
+
+const App4 = () => (
+    <div>
+        <SimpleAnonymousThreatComponent
+            name={robertFromNY.name}
+            city={robertFromNY.city} />
+    </div>
+)
+
+ReactDOM.render(<App4 />, document.getElementById('root'))
